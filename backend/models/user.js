f9668df7d7576dcc2803175e3b93727eb80ff35a
msnmz <snmez.muharrem@gmail.com>
2020-04-16 17:53:03 +0200

    Adds relation between user & place

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,11 +5,11 @@ const Schema = mongoose.Schema;
 
 
 const userSchema = new Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, minlength: 6 },
-    image: { type: String, required: true },
-    places: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
+  places: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Place' }]
 });
 
 userSchema.plugin(uniqueValidator);
